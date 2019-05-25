d7921ea446d8d80416d051c604b40e9349c8c1a8
w-albert <al.wang.me@gmail.com>
2019-05-24 21:11:54 -0700

    Finalize aggregate table function

--- scripts/aggregate_table.R
+++ scripts/aggregate_table.R
@@ -13,61 +13,22 @@ You must also describe why you included the table, and what information it revea
 library(dplyr)
 data <- read.csv("../data/2018_2017_combined.csv", stringsAsFactors = F)
 
-# Function to match key integer with Pitch Scale
-match_key <- function(col) {
-  case_when(
-    col == 0  ~"C",
-    col == 1  ~"C#/Db",
-    col == 2  ~"D",
-    col == 3  ~"D#/Eb",
-    col == 4  ~"E",
-    col == 5  ~"F",
-    col == 6  ~"F#/Gb",
-    col == 7  ~"G",
-    col == 8  ~"G#/Ab",
-    col == 9  ~"A",
-    col == 10 ~"A#/Bb",
-    col == 11 ~"B"
-  )
-}
 
-# Function to add pitch column
-add_key <- function(df) {
-  df %>% 
-    mutate("Pitch" = match_key(df$key))
-}
 
 
 # Function to create table with summary info (grouped by genre)
 summarize_genre_table <- function(df) {
-  with_key <- add_key(df)
-  summarized <- with_key %>% 
+  table <- df %>% 
     group_by(genre) %>%
     summarize(
       "Example" = paste0(first(name), " -- by ", first(artists), ""),
       "Number" = n(),
-      "Pitch" = count(with_key, Pitch) %>% 
-                      arrange(-n) %>%
-                      filter(n == max(n)) %>% 
-                      pull(Pitch)
-        ,
       "Valence (avg)" = round(mean(valence),2),
       "Tempo (avg)" = round(mean(tempo),2),
       "Danceability (avg)" = round(mean(danceability),2),
-      "Energy (avg)" = round(mean(energy),2),
-      "Acousticness (avg)" = round(mean(acousticness),2),
-      "Instrumentalness (avg)" = round(mean(instrumentalness),2),
-      "Percentage Major" = round(sum(mode) / Number * 100, 2)
+      "Energy (avg)" = round(mean(energy),2)
   ) %>% 
-    arrange(-Number)
+    arrange(-Number) %>% 
+    rename(Genre = genre)
+  return(table)
 }
-
-most_common_pitch <- function(df) {
-  df %>% 
-    count(Pitch) %>% 
-    filter(n == max(n)) %>% 
-    pull(Pitch)
-}
-
-
-table <- summarize_key_table(data)

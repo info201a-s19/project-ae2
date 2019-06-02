e619939591a1dcc5e07c838aee4af03647939bf9
Anuradha Seshan <aseshan@uw.edu>
2019-06-02 16:00:08 -0700

    v1

--- scripts/app_ui.R
+++ scripts/app_ui.R
@@ -1,5 +1,64 @@
 library("shiny")
 library("ggplot2")
 
+page_one <- tabPanel(
+  "",
+  titlePanel(""),
+  sidebarLayout(
+    sidebarPanel(
+      # Widget to choose variable for analysis on y axis
+      selectInput(
+        inputId = "",
+        label = "",
+        choices = colnames_fixed_plot
+      ),
+      # Widget to choose size of points on total population statistics plot
+      sliderInput(
+        inputId = "",
+        label = h3(""),
+        min = 0,
+        max = 10,
+        value = 5
+      )
+    ),
+    # total population statistics plot output
+    mainPanel(
+      plotOutput(outputId = "", width = "100%", height = "700px"),
+      verbatimTextOutput(outputId = "")
+    )
+  )
+)
 
-my_ui <- fluidPage(
+
+# Poverty Statistics Page
+page_two <- tabPanel(
+  "",
+  titlePanel(""),
+  sidebarLayout(
+    sidebarPanel(
+      # Widget to choose poverty statistic for analyis on y axis
+      radioButtons("",
+                   label = h3(""),
+                   choices = c()
+      ),
+      # Widget to choose size of bars on poverty statistics plot
+      sliderInput(
+        inputId = "",
+        label = "",
+        min = 0, max = 1, value = 0.5
+      )
+    ),
+    # poverty statistics plot output
+    mainPanel(
+      plotOutput(outputId = "", width = "100%", height = "700px")
+    )
+  )
+)
+
+# Define UI for application
+ui <- navbarPage(
+  "",
+  page_one,
+  page_two
+)
+

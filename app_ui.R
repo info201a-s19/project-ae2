ebb58f859f056d0e009ae25037079d8dee364811
Alice Zhu <alicezhu2010@yahoo.com>
2019-06-04 15:07:46 -0700

    added text to histogram

--- app_ui.R
+++ app_ui.R
@@ -97,7 +97,38 @@ artist_page <-  tabPanel(
       )
     ),
     mainPanel(
-      plotlyOutput("histogram")
+      p("To further understand why the top artists had so many songs in the charts, 
+        we decided to analyze common trends among the features for their songs by finding
+        the feature mean for the songs each top artist had. There are thirteen artists total in the 'Top 10' 
+        artists because the last 9 artists all have four songs in the charts. The histogram below compares the 
+        average of the features of the songs each artist had in the 2017 and 2018 charts. The type of feature
+        for comparison can be chosen by the radio buttons and each color represents a different artist "),
+      plotlyOutput("histogram"),
+      h4(strong("Feature explanations")),
+      p(strong("Danceability: "), "Danceability describes how suitable a track is for dancing based on a combination of musical elements
+        including tempo, rhythm stability, beat strength, and overall regularity. 
+        A value of 0.0 is least danceable and 1.0 is most danceable.",
+        br(), strong("Energy: "), "Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. 
+        Typically, energetic tracks feel fast, loud, and noisy",
+        br(), strong("Key: "), "The estimated overall key of the track. Integers map to pitches using standard Pitch Class notation.
+        If no key was detected, the value is -1.",
+        br(), strong("Loudness: "), "The overall loudness of a track in decibels (dB).
+        Loudness values are averaged across the entire track and are useful for comparing relative loudness of tracks",
+        br(), strong("Speechiness: "), "Speechiness detects the presence of spoken words in a track. 
+        The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value.", 
+        br(), strong("Acousticness: "),	"A confidence measure from 0.0 to 1.0 of whether the track is acoustic.
+        1.0 represents high confidence the track is acoustic.",
+        br(), strong("Instrumentalness: "), "Predicts whether a track contains no vocals. 'Ooh' and 'aah' sounds are treated as instrumental in this context.
+        Rap or spoken word tracks are clearly 'vocal'. The closer the instrumentalness value is to 1.0, 
+        the greater likelihood the track contains no vocal content. ",
+        br(), strong("Liveness: "), "Detects the presence of an audience in the recording.
+        Higher liveness values represent an increased probability that the track was performed live.
+        A value above 0.8 provides strong likelihood that the track is live.",
+        br(), strong("Valence: "),	"A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. 
+        Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), 
+        while tracks with low valence sound more negative (e.g. sad, depressed, angry)",
+        br(), strong("Tempo: "), "The overall estimated tempo of a track in beats per minute (BPM)",
+        br(), strong("Duration: "), "The duration of the track in milliseconds")
     )
   )
 )

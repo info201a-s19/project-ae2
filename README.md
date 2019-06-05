749d1a334333f32858a1a971484ffc253d2ad6ce
Anuradha Seshan <aseshan@uw.edu>
2019-06-05 01:20:23 -0700

    add shiny link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+#SHINY APP LINK
+https://aseshan.shinyapps.io/project-ae2/
 # Final Project
 Use this `REAMDE.md` file to describe your final project (as detailed on Canvas).
 
--- app_ui.R
+++ app_ui.R
@@ -108,7 +108,54 @@ genre_page <- tabPanel(
         and minimum."),
       plotlyOutput(
         outputId = "box_plot", width = "85%",
-        height = "600px", inline = F
+        height = "400px", inline = F
+      ),
+      h4(strong("Feature explanations"),
+         style = "font-family: 'Open Sans Condensed', sans-serif;
+       font-weight: 1000; line-height: 1.1;
+       color: #4d3a7d;"
+      ),
+      p(
+        strong("Danceability: "), "Danceability describes how suitable a track
+is for dancing based on a combination of musical elements
+        including tempo, rhythm stability, beat strength, and overall
+regularity.
+        A value of 0.0 is least danceable and 1.0 is most danceable.",
+        br(), strong("Energy: "), "Energy is a measure from 0.0 to 1.0 and
+represents a perceptual measure of intensity and activity.
+        Typically, energetic tracks feel fast, loud, and noisy",
+        br(), strong("Loudness: "), "The overall loudness of a track in
+decibels (dB).
+        Loudness values are averaged across the entire track and are
+        useful for comparing relative loudness of tracks",
+        br(), strong("Speechiness: "), "Speechiness detects the
+presence of spoken words in a track.
+        The more exclusively speech-like the recording (e.g. talk show,
+        audio book, poetry), the closer to 1.0 the attribute value.",
+        br(), strong("Acousticness: "), "A confidence measure from
+0.0 to 1.0 of whether the track is acoustic.
+        1.0 represents high confidence the track is acoustic.",
+        br(), strong("Instrumentalness: "), "Predicts whether a track
+contains no vocals. 'Ooh' and 'aah' sounds are treated as instrumental in this
+context.
+        Rap or spoken word tracks are clearly 'vocal'. The closer
+the instrumentalness value is to 1.0,
+        the greater likelihood the track contains no vocal content. ",
+        br(), strong("Liveness: "), "Detects the presence of an
+audience in the recording.
+        Higher liveness values represent an increased
+probability that the track was performed live.
+        A value above 0.8 provides strong likelihood
+        that the track is live.",
+        br(), strong("Valence: "), "A measure from 0.0 to 1.0
+describing the musical positiveness conveyed by a track.
+        Tracks with high valence sound more positive (e.g.
+happy, cheerful, euphoric),
+        while tracks with low valence sound more negative
+        (e.g. sad, depressed, angry)",
+        br(), strong("Tempo: "), "The overall estimated tempo of a track
+        in beats per minute (BPM)",
+        br(), strong("Duration: "), "The duration of the track in milliseconds"
       )
     )
   )
